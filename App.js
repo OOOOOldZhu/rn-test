a5a0d17c5c6b16a49e60fd30d4a2050d929d713d
朱海文 <1032671220@qq.com>
2020-07-12 23:38:57 +0800

    app.js

--- App.js
+++ App.js
@@ -1,125 +1,174 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
 
-import React from 'react';
-import {
-    SafeAreaView,
-    StyleSheet,
-    ScrollView,
-    View,
-    Text,
-    Button,
-    StatusBar,
-    DeviceEventEmitter,
-} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View, TextInput, Platform, NativeEventEmitter } from "react-native";
+import { Recognizer, Synthesizer, SpeechConstant } from "react-native-speech-iflytek";
+import Button from "react-native-button";
 
-import {
-    Header,
-    LearnMoreLinks,
-    Colors,
-    DebugInstructions,
-    ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-// //添加 PermissionsAndroid RN自带的
-import { PermissionsAndroid } from 'react-native';
-//就举一个例子 记得加上async异步
-let  requestReadPermission = async ()=> {
-    try {
-        //返回string类型
-        const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-            PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-            PermissionsAndroid.PERMISSIONS.ACCESS_NETWORK_STATE,
-            PermissionsAndroid.PERMISSIONS.ACCESS_WIFI_STATE,
-            PermissionsAndroid.PERMISSIONS.CHANGE_NETWORK_STATE,
-            PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE,
-            PermissionsAndroid.PERMISSIONS.WRITE_SETTINGS,
-            {
-                //第一次请求拒绝后提示用户你为什么要这个权限
-                'title': '我要读写权限',
-                'message': '没权限我不能工作，同意就好了'
-            }
-        )
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            //this.show("你已获取了读写权限")
-        } else {
-            //this.show("获取读写权限失败")
-        }
-    } catch (err) {
-        //this.show(err.toString())
+    if (Platform.OS === 'android') {
+      Synthesizer.init("57c7c5b0");
+      Recognizer.init("57c7c5b0");
+    } else if (Platform.OS === 'ios') {
+      Synthesizer.init("59a4161e");
+      Recognizer.init("59a4161e");
     }
-};
-requestReadPermission();
 
-let result = '返回结果';
+    this.state = {
+      text: "",
+      recordBtnText: "Press to record"
+    };
 
-// import RNVoicesdk from 'voicesdk';
-let init =()=>{
+    this.onRecordStart = this.onRecordStart.bind(this);
+    this.onRecordEnd = this.onRecordEnd.bind(this);
+    this.onRecordCancel = this.onRecordCancel.bind(this);
+    this.onRecognizerResult = this.onRecognizerResult.bind(this);
+    this.onRecognizerError = this.onRecognizerError.bind(this);
+    this.onRecognizerVolumeChanged = this.onRecognizerVolumeChanged.bind(this);
+    this.onSyntheBtnPress = this.onSyntheBtnPress.bind(this);
+    this.onIsSpeakingBtnPress = this.onIsSpeakingBtnPress.bind(this);
+    this.onResumeBtnPress = this.onResumeBtnPress.bind(this);
+  }
 
-}
-let onStart = () => {
-    console.log('1');
-    //RNVoicesdk.startRecognizer();
-};
-let onStop = () => {
-    console.log('2');
-    //RNVoicesdk.stopRecognizer();
-};
-const App: () => React$Node = () => {
+  componentDidMount() {
+    this.recognizerEventEmitter = new NativeEventEmitter(Recognizer);
+    this.recognizerEventEmitter.addListener('onRecognizerResult', this.onRecognizerResult)
+    this.recognizerEventEmitter.addListener('onRecognizerError', this.onRecognizerError)
+    this.recognizerEventEmitter.addListener('onRecognizerVolumeChanged', this.onRecognizerVolumeChanged)
 
-    //console.log('RNVoicesdk => ', RNVoicesdk);
-    DeviceEventEmitter.addListener('result', (e) => {
-        console.log('接收到通知 => '+e);
-        result = e.toString();
-    });
-    init();
-    
+    this.synthesizerEventEmitter = new NativeEventEmitter(Synthesizer);
+    this.synthesizerEventEmitter.addListener('onSynthesizerSpeakCompletedEvent', this.onSynthesizerSpeakCompletedEvent);
+    this.synthesizerEventEmitter.addListener('onSynthesizerBufferCompletedEvent', this.onSynthesizerBufferCompletedEvent);
+  }
+
+  componentWillUnmount() {
+    this.recognizerEventEmitter.removeAllListeners();
+    this.synthesizerEventEmitter.removeAllListeners();
+  }
+
+  render() {
     return (
-        <>
-            <StatusBar barStyle="dark-content"/>
-            <SafeAreaView>
-                <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
-                    <View style={{height: 30}}/>
-                    <Button
-                        containerStyle={styles.containerStyle}
-                        style={{color: 'white'}}
-                        onPress={onStart}
-                        onResponderTerminationRequest={() => true}
-                        title='点击按钮开始识别'
-                    />
-                    <View style={{height: 30}}/>
-                    <Button
-                        containerStyle={styles.containerStyle}
-                        style={{color: 'white'}}
-                        onPress={onStop}
-                        title='停止识别'
-                    />
-                    <View style={{height: 30}}/>
-                    <Text>{result}</Text>
-
-                </ScrollView>
-            </SafeAreaView>
-        </>
+      <View style={styles.container} onStartShouldSetResponder={() => true}>
+        <TextInput onChangeText={text => this.setState({ text })} value={this.state.text} />
+        <Button
+          containerStyle={styles.containerStyle}
+          style={{ color: "white" }}
+          onPress={this.onRecordEnd}
+          onPressIn={this.onRecordStart}
+          activeOpacity={0.8}
+          onResponderTerminationRequest={() => true}
+          onResponderTerminate={this.onRecordCancel}
+        >
+          {this.state.recordBtnText}
+        </Button>
+        <Button containerStyle={styles.containerStyle} style={{ color: "white" }} onPress={this.onSyntheBtnPress} activeOpacity={0.8}>
+          Tap to speak
+        </Button>
+        <Button containerStyle={styles.containerStyle} style={{ color: "white" }} onPress={this.onIsSpeakingBtnPress} activeOpacity={0.8}>
+          is Speaking?
+        </Button>
+        <Button containerStyle={styles.containerStyle} style={{ color: "white" }} onPress={this.onPauseBtnPress} activeOpacity={0.8}>
+          Pause
+        </Button>
+        <Button containerStyle={styles.containerStyle} style={{ color: "white" }} onPress={this.onResumeBtnPress} activeOpacity={0.8}>
+          Resume
+        </Button>
+      </View>
     );
-};
+  }
+
+  onRecordStart() {
+    this.setState({ recordBtnText: "Release to stop" });
+
+    Recognizer.start();
+  }
+
+  onRecordEnd() {
+    this.setState({ recordBtnText: "Press to record" });
+    Recognizer.stop();
+  }
+
+  onRecordCancel(evt) {
+    // setTimeout(() => {
+    //   Recognizer.cancel();
+    // }, 500);
+  }
+
+  onRecognizerResult(e) {
+    if (!e.isLast) {
+      return;
+    }
+    this.setState({ text: e.result });
+  }
+
+  onRecognizerError(result) {
+    if (result.errorCode !== 0) {
+      alert(JSON.stringify(result));
+    }
+  }
+
+  onRecognizerVolumeChanged() {
+
+  }
+
+  async onSyntheBtnPress() {
+    Synthesizer.start(this.state.text);
+  }
+
+  async onPauseBtnPress() {
+    Synthesizer.pause();
+  }
+
+  onResumeBtnPress() {
+    Synthesizer.resume();
+  }
+
+  async onIsSpeakingBtnPress() {
+    let isSpeaking = await Synthesizer.isSpeaking();
+    alert(isSpeaking);
+  }
+
+  onSynthesizerSpeakCompletedEvent(result) {
+    alert('onSynthesizerSpeakCompletedEvent\n\n' + JSON.stringify(result));
+  }
+
+  onSynthesizerBufferCompletedEvent(result) {
+    // alert('onSynthesizerBufferCompletedEvent\n\n' + JSON.stringify(result));
+  }
+}
 
 const styles = StyleSheet.create({
-    scrollView: {
-        backgroundColor: Colors.lighter,
-    },
-    containerStyle: {
-        backgroundColor: '#0275d8',
-        margin: 4,
-        padding: 4,
-        borderRadius: 2,
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "stretch",
+    padding: 5
+  },
+  result: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  recordBtn: {
+    height: 34,
+    fontSize: 16,
+    textAlign: "center",
+    textAlignVertical: "center",
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "#ccc"
+  },
+  containerStyle: {
+    backgroundColor: "#0275d8",
+    margin: 4,
+    padding: 4,
+    borderRadius: 2
+  }
 });
-
-export default App;
