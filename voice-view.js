b3bcd45a0808faad7a366bfb1b0138d9ce325c26
朱海文 <1032671220@qq.com>
2020-07-13 15:20:32 +0800

    增加识别过程中的弹窗

--- voice-view.js
+++ voice-view.js
@@ -8,29 +8,45 @@ import {
     Button,
     Colors
 } from 'react-native';
+import Dialog from "react-native-dialog";
+
 import sdk from './sdk.js';
-sdk.requestPerssion((isErr)=>{});
+sdk.requestPerssion((isErr) => { });
 
 export default class VoiceView extends Component {
-    
+
     constructor(props) {
         super(props)
         // bindAll(this, [
         //     'onStart'
         // ]);
-        this.state = { result: '暂无结果' }
+        this.state = {
+            result: '暂无结果',
+            isShowing: false
+        }
         sdk.initRecognizer();
     }
     render() {
 
         return <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
+            <Dialog.Container visible={this.state.isShowing}>
+                <Dialog.Title>提示</Dialog.Title>
+                <Dialog.Description>
+                    语音正在识别，识别结束后会自动关闭!
+                    </Dialog.Description>
+                {/* <Dialog.Button label="Cancel" />
+                <Dialog.Button label="Delete" /> */}
+            </Dialog.Container>
+
             <View style={{ height: 30 }} />
+
             <Button
                 containerStyle={styles.containerStyle}
                 style={{ color: 'white' }}
                 onPress={() => {
-                    sdk.startRecognizer((jsonObj,jsonString)=> {
-                        this.setState({ result: ''+jsonString })
+                    this.setState({ isShowing: true })
+                    sdk.startRecognizer((jsonObj, jsonString) => {
+                        this.setState({ isShowing: false, result: '' + jsonString })
                     });
                 }}
                 onResponderTerminationRequest={() => true}
