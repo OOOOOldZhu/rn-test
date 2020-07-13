151a4e4747c508ce7c385741b90f10dad7820ba3
朱海文 <1032671220@qq.com>
2020-07-13 10:52:32 +0800

    抽取sdk.js

--- sdk.js
+++ sdk.js
@@ -1,4 +1,61 @@
 
+import { PermissionsAndroid } from 'react-native';
+import RNVoicesdk from 'voicesdk';
+
+class SDK{
+    constructor(){
+        this.init.bind(this);
+        this.initRecognizer.bind(this);
+        this.startRecognizer.bind(this);
+        this.stopRecognizer.bind(this);
+        this.release.bind(this);
+    }
+    //function a(){};
+    async init() {
+        try {
+            //返回string类型
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+                PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+                PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+                PermissionsAndroid.PERMISSIONS.ACCESS_NETWORK_STATE,
+                PermissionsAndroid.PERMISSIONS.ACCESS_WIFI_STATE,
+                PermissionsAndroid.PERMISSIONS.CHANGE_NETWORK_STATE,
+                PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE,
+                PermissionsAndroid.PERMISSIONS.WRITE_SETTINGS,
+                {
+                    //第一次请求拒绝后提示用户你为什么要这个权限
+                    'title': '我要读写权限',
+                    'message': '没权限我不能工作，同意就好了'
+                }
+            )
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log("你已获取了读写权限")
+                this.initRecognizer();
+            } else {
+                //this.show("获取读写权限失败")
+            }
+        } catch (err) {
+            //this.show(err.toString())
+        }
+    };
+    initRecognizer(){
+        console.log('initRecognizer()');
+        RNVoicesdk.init();
+    }
+    startRecognizer(callback){
+        RNVoicesdk.startRecognizer(msg => {
+            console.log('js接收的数据 => ' + msg);
+            callback(msg);
+        });
+    }
+    stopRecognizer(){
+        RNVoicesdk.stopRecognizer();
+    }
+    release(){
+        RNVoicesdk.release();
+    }
+}
 // word = 进度管理
 let request  = (word)=>{
     return new Promise((resolve,reject)=>{
@@ -31,3 +88,5 @@ let request  = (word)=>{
 //     result:response.data.result,
 //     action:response.data.action
 // }
+
+export default new SDK();
--- voice-view.js
+++ voice-view.js
@@ -7,67 +7,20 @@ import {
     Text,
     Button,
     Colors
-} from 'react-native'
-
-// //添加 PermissionsAndroid RN自带的
-import { PermissionsAndroid } from 'react-native';
-//就举一个例子 记得加上async异步
-let requestReadPermission = async () => {
-    try {
-        //返回string类型
-        const granted = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-            PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-            PermissionsAndroid.PERMISSIONS.ACCESS_NETWORK_STATE,
-            PermissionsAndroid.PERMISSIONS.ACCESS_WIFI_STATE,
-            PermissionsAndroid.PERMISSIONS.CHANGE_NETWORK_STATE,
-            PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE,
-            PermissionsAndroid.PERMISSIONS.WRITE_SETTINGS,
-            {
-                //第一次请求拒绝后提示用户你为什么要这个权限
-                'title': '我要读写权限',
-                'message': '没权限我不能工作，同意就好了'
-            }
-        )
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            console.log("你已获取了读写权限")
-            init();
-        } else {
-            //this.show("获取读写权限失败")
-        }
-    } catch (err) {
-        //this.show(err.toString())
-    }
-};
-requestReadPermission();
-
-import RNVoicesdk from 'voicesdk';
-let init = () => {
-    RNVoicesdk.init();
-}
-
-let onStop = () => {
-    console.log('2');
-    RNVoicesdk.stopRecognizer();
-};
-
+} from 'react-native';
+import sdk from './sdk.js';
 export default class VoiceView extends Component {
-    // constructor 一般也推荐都写出来
+
     constructor(props) {
         super(props)
-        bindAll(this, [
-            'onStart'
-        ]);
+        // bindAll(this, [
+        //     'onStart'
+        // ]);
         this.state = { result: '暂无结果' }
     }
-    onStart () {
-        console.log('1');
-        RNVoicesdk.startRecognizer(msg => {
-            console.log('js接收的数据 => ' + msg);
-            this.setState({result:msg})
-        });
-    };
+    componentWillMount() {
+        sdk.init();
+    }
     render() {
 
         return <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
@@ -75,7 +28,11 @@ export default class VoiceView extends Component {
             <Button
                 containerStyle={styles.containerStyle}
                 style={{ color: 'white' }}
-                onPress={this.onStart}
+                onPress={() => {
+                    sdk.startRecognizer(res => {
+                        this.setState({ result: res })
+                    });
+                }}
                 onResponderTerminationRequest={() => true}
                 title='点击按钮开始识别'
             />
@@ -83,7 +40,9 @@ export default class VoiceView extends Component {
             <Button
                 containerStyle={styles.containerStyle}
                 style={{ color: 'white' }}
-                onPress={onStop}
+                onPress={() => {
+                    sdk.stopRecognizer();
+                }}
                 title='停止识别'
             />
             <View style={{ height: 30 }} />
